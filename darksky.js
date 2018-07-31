499839a62670ab823a3f9b446ead277596d5615f
aaronzhang1234 <nimbyest@gmail.com>
2018-07-31 16:53:55 -0400

    Sun goes in a half circle animation depending on time

--- darksky.js
+++ darksky.js
@@ -1,7 +1,9 @@
-$(document).ready(() => {
+$(document).ready(() => { var dailyInfo; 
     setInterval(printDate, 1000);
+    setInterval(updateSunLocation, 2000);
     getNewData();
-    setInterval(getNewData, 1000*60*60); var fuckCors = "https://cors-anywhere.herokuapp.com/";
+    setInterval(getNewData, 1000*60*60); 
+    var fuckCors = "https://cors-anywhere.herokuapp.com/";
     function printDate(){
         var today = new Date().toLocaleTimeString();
         $('#time').text(today);
@@ -27,15 +29,41 @@ $(document).ready(() => {
     function errored(){
         console.log("broken");
     }
+    function updateSunLocation(){
+        $('#sunImage').remove();
+        if(sunIsDown(dailyInfo))
+        {
+            setTimeout()
+            var moonImage = "<img id='moonImage' src='images/moon.png'></img>";
+            var centerDiv = $("#sunDisplay");
+            centerDiv.append(moonImage);
+            console.log("night night");
+        }
+        else
+        {
+            var sunPercent = getSunPercentage(dailyInfo);
+            const hypot = 6;
+            if(sunPercent>90){
+                sunPercent = 180 - sunPercent;
+            }
+            const sunLength = Math.cos(sunPercent*(Math.PI/180.0))*hypot;
+            const sunHeight = Math.sqrt((Math.pow(hypot,2)-Math.pow(sunLength,2)));
+            var center = $('#centersun');    
+            var centerPos = center.position();
+            var sunImage = "<img id='sunImage' src='images/sun.jpg'></img>";
+            $('body').append(sunImage); 
+            var killme = $('#sunImage');
+            killme.css("top",centerPos.top-sunHeight*50);
+            killme.css("left", centerPos.left+sunLength*50);
+            console.log(centerPos);
+            console.log(killme.position());
+      }
+    }
     function getForecast(darkSkyURL){
         $.getJSON(darkSkyURL, function(data){
             var currently = data.currently;
             var today = data.daily.data[0];
-            if(sunIsDown(today)){
-                console.log("Nighty Night");
-            }else{
-                console.log("Sun is currently: " + getSunPercentage(today));             
-            }
+            dailyInfo = today;
             console.log(today);
             console.log(currently);            
        });
@@ -46,7 +74,6 @@ $(document).ready(() => {
             var timeElapsed = timeRightNow - todayInfo.sunriseTime;
             var sunpercentage = timeElapsed/totalSunUpness;
             return sunpercentage * 180;
-
     }
     function getSolarNoon(todayInfo){
             var solarnoon = new Date(0);
@@ -54,8 +81,9 @@ $(document).ready(() => {
             console.log(solarnoon);         
     }
     function sunIsDown(todayInfo){
-        var timeRightNow = (new Date().getTime())/1000;
-        if(timeRightNow > todayInfo.sunsetTime || timeRightNow < todayInfo.sunriseTime){
+        var timeRightNow = new Date();
+        var seconds = (timeRightNow.getTime())/1000;
+        if(seconds > todayInfo.sunsetTime || seconds < todayInfo.sunriseTime){
             return true;
         }else{
             return false;
